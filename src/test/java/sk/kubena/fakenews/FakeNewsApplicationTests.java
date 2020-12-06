bc0f9c6f680c8006576a25c7537c812ac4c010ab
JakubKubena <jakubena@gmail.com>
2020-12-06 20:36:40 +0100

    Initial commit

---
+++ src/test/java/sk/kubena/fakenews/FakeNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.kubena.fakenews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
